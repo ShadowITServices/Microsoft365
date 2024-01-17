7d6d9935993e9a2fc841ef115a60f022977f7bbf
Beau Dean <beau.dean@shadowit.com.au>
2024-01-17 11:18:38 +1100

    Create IdentifyOrphanedTeamsSites.ps1

---
+++ Microsoft Teams/IdentifyOrphanedTeamsSites.ps1
@@ -0,0 +1,36 @@
+﻿# Connect to Microsoft Teams
+Connect-MicrosoftTeams
+
+# Connect to SharePoint Online
+Connect-SPOService -Url https://<MicrosoftTenantName>-admin.sharepoint.com
+
+# Fetch all Teams
+$teams = Get-Team
+
+# Fetch all Teams SharePoint Sites
+$teamSites = Get-SPOSite -Template "GROUP#0"
+
+# Create an array to hold the orphaned sites
+$orphanedSites = @()
+
+# Check for orphaned sites
+foreach ($site in $teamSites) {
+    $team = $teams | Where-Object { $_.GroupId -eq $site.GroupId }
+    if (-not $team) {
+        Write-Host "Orphaned site found: $($site.Url)"
+        $members = Get-TeamUser -GroupId $site.GroupId | Select-Object -ExpandProperty User
+
+        # Join members into a single string
+        $membersList = $members -join "; "
+
+        $orphanedSites += New-Object PSObject -Property @{
+            URL = $site.Url
+            Owner = $site.Owner
+            Created = $site.Created
+            Members = $membersList
+        }
+    }
+}
+
+# Export to CSV
+$orphanedSites | Export-Csv -Path "C:\Temp\OrphanedTeamsSitesWithMembers.csv" -NoTypeInformation
